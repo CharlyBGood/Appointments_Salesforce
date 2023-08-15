58b841fa7d27dec96f411b9539a59425b5ee7b9c
Carlos Bonavita <carlos.bonavita@dataart.com>
2023-08-15 09:54:32 -0300

    fix component js to work with apex controller

--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -1,10 +1,22 @@
 import { LightningElement, wire } from "lwc";
-import getContacts from "@salesforce/apex/ContactController.getContacts";
+// import getContacts from "@salesforce/apex/ContactController.getContacts";
+import searchContacts from "@salesforce/apex/ContactController.searchContacts";
 import { NavigationMixin } from "lightning/navigation";
 
 export default class ContactList extends NavigationMixin(LightningElement) {
-  @wire(getContacts)
+  searchText = "";
+
+  @wire(searchContacts, { searchText: "$searchText" })
   contacts;
+
+  handleChange(event) {
+    const searchTextAux = event.target.value;
+    console.log("Texto renderizado: " + searchTextAux);
+    if (searchTextAux.length >= 3 || searchTextAux === "") {
+      this.searchText = searchTextAux;
+      console.log(this.searchText);
+    }
+  }
 
   handleSelect(event) {
     const contactId = event.detail;
--- force-app/main/default/lwc/contactLookup/contactLookup.js
+++ force-app/main/default/lwc/contactLookup/contactLookup.js
@@ -1,11 +1,15 @@
-import { LightningElement } from "lwc";
-// import searchContacts from "@salesforce/apex/ContactController.getContacts";
+import { LightningElement, wire } from "lwc";
+import searchContacts from "@salesforce/apex/ContactController.searchContacts";
 
 export default class ContactLookup extends LightningElement {
   searchText = "";
 
+  @wire(searchContacts, { searchText: "$searchText" })
+  contacts;
+
   handleChange(event) {
-    const searchTextAux = event.detail.value;
+    const searchTextAux = event.target.value;
+    console.log(searchTextAux);
     if (searchTextAux.lenght >= 3 || searchTextAux === "") {
       this.searchText = searchTextAux;
     }
